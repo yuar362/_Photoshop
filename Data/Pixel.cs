51746f824b3de3361a87c643f7521e87cc7fa833
Юрий Аношин <uanoshin@cdezign.ru>
2018-04-20 15:18:26 +0300

    дублирование кода

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -7,49 +7,32 @@ namespace MyPhotoshop
 {
     public class Pixel
     {
+        double check(double value_)
+        {
+            if (value_ < 0 || value_ > 1)
+                throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", value_));
+            return value_;
+        }
+
         double r;
         public double R
         {
-            get
-            {
-                return r;
-            }
-            set
-            {
-                if (value < 0 || value > 1)
-                    throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", value));
-                r = value;
-            }
+            get { return r; }
+            set { r = check(value); }
         }
 
         double g;
         public double G
         {
-            get
-            {
-                return g;
-            }
-            set
-            {
-                if (value < 0 || value > 1)
-                    throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", value));
-                g = value;
-            }
+            get { return r; }
+            set { g = check(value); }
         }
 
         double b;
         public double B
         {
-            get
-            {
-                return b;
-            }
-            set
-            {
-                if (value < 0 || value > 1)
-                    throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", value));
-                b = value;
-            }
+            get { return r; }
+            set { b = check(value); }
         }
     }
 }
