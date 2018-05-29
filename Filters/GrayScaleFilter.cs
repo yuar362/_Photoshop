ed7374bf941073c105b958f14304d15ec9e574e9
Yuriy <yuar362@yandex.ru>
2018-05-29 20:43:31 +0300

    новый фильтр оттенки серого

---
+++ Filters/GrayScaleFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class GrayScaleFilter : IFilter
+    {
+        public ParameterInfo[] GetParameters()
+        {
+            return new ParameterInfo[0];
+        }
+
+        public override string ToString()
+        {
+            return "Оттенки серого";
+        }
+
+        public Photo Process(Photo original, double[] parameters)
+        {
+            var result = new Photo(original.width, original.height);
+
+            for (int x = 0; x < result.width; x++)
+                for (int y = 0; y < result.height; y++)
+                {
+                    var lightness = original[x, y].R + original[x, y].G + original[x, y].B;
+                    lightness *= 0.3333;
+                    result[x, y] = new Pixel(lightness, lightness, lightness);
+                }
+            return result;
+        }
+		
+    }
+}
